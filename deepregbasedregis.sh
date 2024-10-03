5e2827f6f4d47e75fe4f39972b88b4b27834c989
dharlabwustl <atulkumar@wustl.edu>
2024-10-03 13:06:41 -0500

    initial commit

--- deepregbasedregis.sh
+++ deepregbasedregis.sh
@@ -326,7 +326,7 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
       betfilename=${inv_file_basename%_scct_strippedResampled1lin1Inv.mat}.nii.gz
       templatefile_after_linear_transformation=${template_T_OUTPUT_dir}/${template_file%.nii*}${betfilename}
       echo templatefile_after_linear_transformation::${templatefile_after_linear_transformation}
-      target_bet_grayscale=${betfilename}
+      target_bet_grayscale=${working_dir}/${betfilename}
       echo target_bet_grayscale:${target_bet_grayscale}
 #      /opt/conda/envs/deepreg/bin/python3 create_datah5files_May24_2023.py ${templatefile_after_linear_transformation} ${target_bet_grayscale}
       ## make the h5 file which will contain both target and template data along with its corresponding masks.

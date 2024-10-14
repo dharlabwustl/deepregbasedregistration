f07f3ebd0400be71b88daa8e163007275213838d
dharlabwustl <atulkumar@wustl.edu>
2024-10-14 16:09:44 -0500

    initial commit

--- deepregapplication.sh
+++ deepregapplication.sh
@@ -339,6 +339,8 @@ echo "LINEAR REGISTRATION TO TEMPLATE"
 
 template_directory=/software/
 template_file=${template_directory}/'scct_strippedResampled1.nii.gz'
-
-
 /opt/conda/envs/deepreg/bin/python3 create_datah5files_May24_2023.py ${this_mri_filename_brain_bet_gray} ${template_file}
+cp -r /rapids/notebooks/DeepReg /software/
+cp /software/data.h5 /software/DeepReg/demos/classical_mr_prostate_nonrigid/dataset/
+cp /software/demo_register_batch_atul.py /software/DeepReg/demos/classical_mr_prostate_nonrigid/
+/opt/conda/envs/deepreg/bin/python3 /software/demo_register_batch_atul.py /software/DeepReg/demos/classical_mr_prostate_nonrigid/dataset/data.h5 ${output_directory}

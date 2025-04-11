6a078d720c4649e92f2d863e944dc74cf40b009a
dharlabwustl <atulkumar@wustl.edu>
2025-04-11 15:05:41 -0500

    ehii

--- deepregbasedregis_location_v2.sh
+++ deepregbasedregis_location_v2.sh
@@ -394,8 +394,8 @@ rm ${working_dir}/warped_1*
 #      uploadsinglefile ${sessionID} ${scanID} ${mask_binary_output_dir} ${snipr_output_foldername} ${mask_binary_output_filename}
 #      uploadsinglefile ${sessionID} ${scanID} ${mask_binary_output_dir} ${snipr_output_foldername} ${mask_binary_output_filename}
             snipr_output_foldername="PREPROCESS_SEGM"
-            all_warped_files=$(find ${working_dir} -name 'warped*') ## 'warped'*${template_prefix}*'.nii.gz')
-            for eachfile in ${working_dir}/warped*.nii*; #${all_warped_files};
+            all_warped_files=$(find ${working_dir_1} -name 'warped*') ## 'warped'*${template_prefix}*'.nii.gz')
+            for eachfile in ${working_dir_1}/warped*.nii*; #${all_warped_files};
             do
               echo ${eachfile}
               uploadsinglefile ${sessionID} ${scanID} $(dirname ${eachfile}) ${snipr_output_foldername} $(basename ${eachfile} )

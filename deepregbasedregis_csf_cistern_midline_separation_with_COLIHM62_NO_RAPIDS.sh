3c97f2d67aa514bd76c0b5e04c884370154f83aa
dharlabwustl <atulkumar@wustl.edu>
2025-06-03 16:05:46 -0500

    ehii

--- deepregbasedregis_csf_cistern_midline_separation_with_COLIHM62_NO_RAPIDS.sh
+++ deepregbasedregis_csf_cistern_midline_separation_with_COLIHM62_NO_RAPIDS.sh
@@ -364,8 +364,8 @@ rm ${working_dir}/warped_1*
 #####################################################################################################################
 
       python3 create_datah5files_May24_2023.py ${moving_image} ${fixed_image}
-##      mkdir /rapids/notebooks/DeepReg/demos/classical_mr_prostate_nonrigid/dataset
-      cp -r /rapids/notebooks/DeepReg /software/
+##      mkdir /workspace/deepreg/DeepReg/demos/classical_mr_prostate_nonrigid/dataset
+      cp -r /workspace/deepreg/DeepReg /software/
       cp /software/data.h5 /software/DeepReg/demos/classical_mr_prostate_nonrigid/dataset/
       cp /software/demo_register_batch_atul.py /software/DeepReg/demos/classical_mr_prostate_nonrigid/
       if [ ! -f /workinginput/fixed_image.nii.gz  ]; then

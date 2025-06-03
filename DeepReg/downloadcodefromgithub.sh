6d8203c6d308f12466c3615330416e509609d6b9
dharlabwustl <atulkumar@wustl.edu>
2025-06-03 16:19:43 -0500

    ehii

--- DeepReg/downloadcodefromgithub.sh
+++ DeepReg/downloadcodefromgithub.sh
@@ -6,7 +6,8 @@ y=${git_link%.git}
 git_dir=$(basename $y)
 mv ${git_dir}/* /software/
 chmod +x /software/*.sh 
-cp -r /home/rapids/DeepReg/deepreg /software/
+#cp -r /home/rapids/DeepReg/deepreg /software/
+cp -r  /software/deepreg /software/
 SESSION_ID=${1}
 XNAT_USER=${2}
 XNAT_PASS=${3}
